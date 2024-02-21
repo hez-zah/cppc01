dc7998ccb0c6b76babc702a2783f6a7228c145fd
hez-zah <124633794+hez-zah@users.noreply.github.com>
2024-02-21 19:23:10 +0100

    Add files via upload

---
+++ CPP01/ex00/Zombie.hpp
@@ -0,0 +1,20 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+
+class Zombie
+{
+    private :
+        string  name;
+    public :
+        void announce();
+};
+
+#endif
---
+++ CPP01/ex00/main.cpp
@@ -0,0 +1,6 @@
+#include "Zombie.hpp"
+
+int main()
+{
+    cout << "hello" << endl;
+}
